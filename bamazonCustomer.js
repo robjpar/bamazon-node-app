6a8c6d2971f1bdaa6567228600a88cc59f47ffcf
Robert J. Paruch <roobbb@live.com>
2019-04-16 03:28:54 -0700

    Improved the "customer" code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,64 +19,93 @@ mysqlConnection.connect((error) => {
   if (error) {
     print(`!!! Could not connect: ${error}`);
   } else {
+    print('');
     print(`>>> Connected to: ${mysqlConnection.config.database}`);
-    showInventory();
+    print('');
+    showOptions();
   }
 });
 
-const showInventory = () => {
-  mysqlConnection.query(
-    'SELECT item_id, product_name, price, stock_quantity, product_sales FROM products',
-    (error, inventory) => {
-      if (error) {
-        print(`!!! Could not query: ${error}`);
+const showOptions = () => {
+  inquirer
+    .prompt([{
+      name: 'option',
+      type: 'list',
+      message: 'Choose an option:',
+      choices: [
+        'View inventory',
+        'Place an order',
+        new inquirer.Separator(),
+        'Quit'
+      ],
+    }])
+    .then((answers) => {
+      if (answers.option === 'View inventory') {
+        showInventory();
+      } else if (answers.option === 'Place an order') {
+        placeOrder();
       } else {
-        console.table('\nItems available for sale', inventory.map((item) => { // the stock quantity and product sales are not displayed in the table
-          return {
-            item_id: item.item_id,
-            product_name: item.product_name,
-            price: item.price,
-          };
-        }));
-        showOptions(inventory);
+        mysqlConnection.end();
       }
     });
 };
 
-const showOptions = (inventory) => {
-  print('\nStarting a new order...');
-  inquirer
-    .prompt([{
-      name: 'id',
-      type: 'number',
-      message: 'ID of the item being ordered',
-      validate: (value) => /^\d+$/.test(value) && value >= 1 && value <= inventory.length, // one integer, at least one digit, within the range
-    }, {
-      name: 'quantity',
-      type: 'number',
-      message: 'How many units',
-      validate: (value) => /^\d+$/.test(value), // one integer, at least one digit
-    }])
-    .then((answers) => {
-      if (answers.quantity > inventory[answers.id - 1].stock_quantity) {
-        print('\nInsufficient quantity!!!');
-        showOptions(inventory);
+const showInventory = () => {
+  mysqlConnection.query(
+    'SELECT item_id, product_name, price FROM products',
+    (error, inventory) => {
+      if (error) {
+        print(`!!! Could not query: ${error}`);
       } else {
-        const orderTotal = answers.quantity * inventory[answers.id - 1].price;
-        const remainingQuantity = inventory[answers.id - 1].stock_quantity - answers.quantity;
-        const totalSales = inventory[answers.id - 1].product_sales + orderTotal;
+        console.table('>>> Inventory', inventory);
+        showOptions();
+      }
+    });
+};
 
-        mysqlConnection.query(
-          'UPDATE products SET stock_quantity = ?, product_sales = ? WHERE item_id = ?', [remainingQuantity, totalSales, answers.id],
-          (error) => {
-            if (error) {
-              print(`!!! Could not query: ${error}`);
+const placeOrder = () => {
+  mysqlConnection.query(
+    'SELECT product_name, price, stock_quantity, product_sales FROM products',
+    (error, inventory) => {
+      if (error) {
+        print(`!!! Could not query: ${error}`);
+      } else {
+        inquirer
+          .prompt([{
+            name: 'id',
+            type: 'number',
+            message: 'ID of the item being ordered',
+            validate: (value) => /^\d+$/.test(value) && value >= 1 && value <= inventory.length, // one integer, at least one digit, within the range
+          }, {
+            name: 'quantity',
+            type: 'number',
+            message: 'How many units',
+            validate: (value) => /^\d+$/.test(value), // one integer, at least one digit
+          }])
+          .then((answers) => {
+            if (answers.quantity > inventory[answers.id - 1].stock_quantity) {
+              print('>>> Insufficient quantity!!!');
+              print('');
+              showOptions();
+            } else {
+              const orderTotal = answers.quantity * inventory[answers.id - 1].price;
+              const remainingQuantity = inventory[answers.id - 1].stock_quantity - answers.quantity;
+              const totalSales = inventory[answers.id - 1].product_sales + orderTotal;
+
+              mysqlConnection.query(
+                'UPDATE products SET stock_quantity = ?, product_sales = ? WHERE item_id = ?', [remainingQuantity, totalSales, answers.id],
+                (error) => {
+                  if (error) {
+                    print(`!!! Could not query: ${error}`);
+                  }
+                });
+
+              print(`You ordered ${answers.quantity} units of ${inventory[answers.id - 1].product_name}`);
+              print(`Your total is: ${orderTotal.toFixed(2)} `);
+              print('');
+              showOptions();
             }
           });
-        mysqlConnection.end();
-
-        print(`\nYou ordered ${answers.quantity} units of ${inventory[answers.id - 1].product_name}`);
-        print(`Your total is: ${orderTotal.toFixed(2)} `);
       }
     });
 };
